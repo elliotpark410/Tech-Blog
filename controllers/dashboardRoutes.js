cc9485e5a911fb9486767f787ab96b593ae7ca73
Elliot Park <elliotpark410@gmail.com>
2022-02-17 04:58:59 -0800

    create get route for dashboard page

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const postData = await Post.findByPk(req.session.user_id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Comment,
+          attributes: ['comment_content', 'user_id', 'post_id'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('dashboard', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+module.exports = router;
