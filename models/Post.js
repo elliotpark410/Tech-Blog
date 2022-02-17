222f9955c727739774c307b3f03ed9aa17cf4e95
Elliot Park <elliotpark410@gmail.com>
2022-02-17 04:08:55 -0800

    create seed files for userData, commentData, and postData

--- models/Post.js
+++ models/Post.js
@@ -25,11 +25,6 @@ Post.init(
         key: 'id',
       },
     },
-    date_created: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
   },
   {
     sequelize,
