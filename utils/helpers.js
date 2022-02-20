00a117e27bf75b2ba49fef8761e00d6b39c73c29
Elliot Park <elliotpark410@gmail.com>
2022-02-20 05:03:40 -0800

    create homepage.handlebars view

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,6 @@
-// add current date function
+module.exports = {
+  format_date: date => {
+      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    },
+
+}
